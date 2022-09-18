35d9f4eb8a41fed13b70717a47ce341716486724
Allen Gueco <allen.gueco1@gmail.com>
2022-09-18 16:26:23 -0400

    AR-5: basic query

--- libs/ray/src/lib.rs
+++ libs/ray/src/lib.rs
@@ -6,6 +6,12 @@ mod ray {
         pub origin: Tuple,
         pub dir: Tuple,
     }
+
+    impl Ray {
+        pub fn position<T: Into<f64>>(&self, t: T) -> Tuple {
+            self.origin + self.dir * t.into()
+        }
+    }
 }
 
 pub use ray::*;
@@ -18,6 +24,24 @@ mod tests {
     #[test]
     fn creating_and_querying_a_ray() {
         let origin = point(1, 2, 3);
-        let direction = vector(4, 5, 6);
+        let dir = vector(4, 5, 6);
+
+        let r = Ray { origin, dir };
+
+        assert_eq!(origin, r.origin);
+        assert_eq!(dir, r.dir);
+    }
+
+    #[test]
+    fn computing_point_from_distance() {
+        let r = Ray {
+            origin: point(2, 3, 4),
+            dir: vector(1, 0, 0),
+        };
+
+        assert_eq!(point(2, 3, 4), r.position(0));
+        assert_eq!(point(3, 3, 4), r.position(1));
+        assert_eq!(point(1, 3, 4), r.position(-1));
+        assert_eq!(point(4.5, 3, 4), r.position(2.5));
     }
 }
