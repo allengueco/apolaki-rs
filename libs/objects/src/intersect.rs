1487ec6041a226f3f459f93342c9f96281d91619
Allen Gueco <allen.gueco1@gmail.com>
2023-04-23 15:58:24 -0400

    AR-5: CH 5 done

--- libs/objects/src/intersect.rs
+++ libs/objects/src/intersect.rs
@@ -51,3 +51,82 @@ pub trait Intersect {
     type O: Intersect;
     fn intersect(&self, ray: Ray) -> Option<Intersections<Self::O>>;
 }
+
+#[cfg(test)]
+mod intersections_test {
+    use crate::Sphere;
+    use super::*;
+
+    #[test]
+    fn an_intersection_encapsulates_t_and_object() {
+        let s = Sphere::default();
+
+        let i = Intersection::new(3.5, s);
+
+        assert_eq!(i.t, 3.5);
+        assert_eq!(i.obj, s);
+    }
+
+    #[test]
+    fn aggregating_intersections() {
+        let s = Sphere::default();
+        let i1 = Intersection::new(1, s);
+        let i2 = Intersection::new(2, s);
+
+        let xs = Intersections::from(vec![i1, i2]);
+
+        assert_eq!(xs.len(), 2);
+        assert_eq!(xs[0].t, 1.0);
+        assert_eq!(xs[1].t, 2.0);
+    }
+
+    #[test]
+    fn the_hit_when_all_intersections_have_positive_t() {
+        let s = Sphere::default();
+        let i1 = Intersection::new(1, s);
+        let i2 = Intersection::new(2, s);
+        let xs = Intersections::from(vec![i2, i1]);
+
+        let i = xs.hit();
+
+        assert_eq!(i, Some(&i1));
+    }
+
+    #[test]
+    fn the_hit_when_some_intersections_have_negative_t() {
+        let s = Sphere::default();
+        let i1 = Intersection::new(-1, s);
+        let i2 = Intersection::new(1, s);
+        let xs = Intersections::from(vec![i2, i1]);
+
+        let i = xs.hit();
+
+        assert_eq!(i, Some(&i2));
+    }
+
+    #[test]
+    fn the_hit_when_all_intersections_have_negative_t() {
+        let s = Sphere::default();
+        let i1 = Intersection::new(-2, s);
+        let i2 = Intersection::new(-1, s);
+        let xs = Intersections::from(vec![i2, i1]);
+
+        let i = xs.hit();
+
+        assert_eq!(i, None);
+    }
+
+    #[test]
+    fn the_hit_is_always_the_lowest_nonnegative_intersection() {
+        let s = Sphere::default();
+        let i1 = Intersection::new(5, s);
+        let i2 = Intersection::new(7, s);
+        let i3 = Intersection::new(-3, s);
+        let i4 = Intersection::new(2, s);
+        let xs = Intersections::from(vec![i1, i2, i3, i4]);
+
+        let i = xs.hit();
+
+        assert_eq!(i, Some(&i4));
+    }
+}
--- libs/objects/src/sphere.rs
+++ libs/objects/src/sphere.rs
@@ -1,12 +1,14 @@
-use apolaki_matrix::BaseMatrix;
-use crate::intersect::{Intersect, Intersection, Intersections};
+use apolaki_matrix::{BaseMatrix, Invert};
 use apolaki_ray::Ray;
+use apolaki_transform::Transform;
 use apolaki_tuple::point;
+
+use crate::intersect::{Intersect, Intersection, Intersections};
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Sphere {
     pub radius: f64,
-    pub transform: BaseMatrix<4>
+    pub transform: BaseMatrix<4>,
 }
 
 impl Sphere {
@@ -24,9 +26,10 @@ impl Default for Sphere {
 impl Intersect for Sphere {
     type O = Self;
     fn intersect(&self, ray: Ray) -> Option<Intersections<Self::O>> {
-        let sphere_to_ray = ray.origin - point(0, 0, 0);
-        let a = ray.dir.dot(ray.dir);
-        let b = 2. * ray.dir.dot(sphere_to_ray);
+        let r = ray.transform(self.transform.invert());
+        let sphere_to_ray = r.origin - point(0, 0, 0);
+        let a = r.dir.dot(r.dir);
+        let b = 2. * r.dir.dot(sphere_to_ray);
         let c = sphere_to_ray.dot(sphere_to_ray) - 1.;
 
         let discriminant = b.powi(2) - 4. * a * c;
@@ -41,3 +44,114 @@ impl Intersect for Sphere {
         }
     }
 }
+
+#[cfg(test)]
+mod sphere_tests {
+    use apolaki_ray::Ray;
+    use apolaki_tuple::{point, vector};
+
+    use super::*;
+
+    #[test]
+    fn ray_intersects_a_sphere_at_two_points() {
+        let r = Ray::new(point(0, 0, -5), vector(0, 0, 1));
+        let s = Sphere::default();
+        let xs = s.intersect(r).expect("must have intersections");
+        assert_eq!(xs.len(), 2);
+        assert_eq!(xs[0].t, 4.0);
+        assert_eq!(xs[1].t, 6.0);
+    }
+
+    #[test]
+    fn ray_intersects_a_sphere_at_tangent() {
+        let r = Ray::new(point(0, 1, -5), vector(0, 0, 1));
+        let s = Sphere::default();
+        let xs = s.intersect(r).expect("must have intersections");
+        assert_eq!(xs.len(), 2);
+        assert_eq!(xs[0].t, 5.0);
+        assert_eq!(xs[1].t, 5.0);
+    }
+
+    #[test]
+    fn ray_misses_a_sphere() {
+        let r = Ray::new(point(0, 2, -5), vector(0, 0, 1));
+        let s = Sphere::default();
+        let xs = s.intersect(r);
+        assert_eq!(xs, None);
+    }
+
+    #[test]
+    fn ray_originates_inside() {
+        let r = Ray::new(point(0, 0, 0), vector(0, 0, 1));
+        let s = Sphere::default();
+        let xs = s.intersect(r).expect("must have intersections");
+        assert_eq!(xs.len(), 2);
+        assert_eq!(xs[0].t, -1.0);
+        assert_eq!(xs[1].t, 1.0);
+    }
+
+    #[test]
+    fn sphere_is_behind_ray() {
+        let r = Ray::new(point(0, 0, 5), vector(0, 0, 1));
+        let s = Sphere::default();
+        let xs = s.intersect(r).expect("must have intersections");
+        assert_eq!(xs.len(), 2);
+        assert_eq!(xs[0].t, -6.0);
+        assert_eq!(xs[1].t, -4.0);
+    }
+
+    #[test]
+    fn intersect_sets_the_object_on_the_intersection() {
+        let ray = Ray::new(point(0, 0, -5), vector(0, 0, 1));
+        let s = Sphere::default();
+
+        let xs = s.intersect(ray).expect("Must intersect");
+
+        assert_eq!(xs.len(), 2);
+        assert_eq!(xs[0].obj, s);
+        assert_eq!(xs[1].obj, s);
+    }
+
+    #[test]
+    fn a_spheres_default_transform() {
+        let s = Sphere::default();
+
+        assert_eq!(BaseMatrix::identity(), s.transform);
+    }
+
+    #[test]
+    fn changing_a_spheres_transformation() {
+        let mut s = Sphere::default();
+
+        let m = BaseMatrix::identity().translate(2, 3, 4);
+        s.transform = m;
+
+        assert_eq!(BaseMatrix::identity().translate(2, 3, 4), s.transform);
+    }
+
+    #[test]
+    fn intersecting_a_scaled_sphere_with_a_ray() {
+        let r = Ray::new(point(0, 0, -5), vector(0, 0, 1));
+        let mut s = Sphere::default();
+
+        s.transform = BaseMatrix::identity().scale(2, 2, 2);
+        let xs = s.intersect(r).expect("Must intersect");
+
+        assert_eq!(2, xs.len());
+
+        assert_eq!(3.0, xs[0].t);
+        assert_eq!(7.0, xs[1].t);
+    }
+
+    #[test]
+    fn intersecting_a_translated_sphere_with_a_ray() {
+        let r = Ray::new(point(0, 0, -5), vector(0, 0, 1));
+        let mut s = Sphere::default();
+
+        s.transform = BaseMatrix::identity().translate(5, 0, 0);
+        let xs = s.intersect(r);
+
+        assert_eq!(None, xs);
+    }
+}
+
